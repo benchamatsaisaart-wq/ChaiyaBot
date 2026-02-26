a52faec3218a1934ba74075e5afd03b0b5738a99
benchamatsaisaart-wq <benchamatsaisaart@gmail.com>
2026-02-26 09:18:32 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // 1. กลับมาใช้ Key ตัวใหม่ที่ลงท้ายด้วย IZBq0WY
-const API_KEY = "AIzaSyC8568qKqXfNQQAyKPYZukdGNHD_rSLso4";
+const API_KEY = "AIzaSyCR2N9Tdxu44BrECEw8xQlrMrkPqHxhlg0";
 
 const URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
 const chatBox = document.getElementById('chat-box');
